0000d1d66b5fcd6d840a2d9ab4fe5310d2a4ee59
Kristofer Hanes <kristoferhanes@gmail.com>
2017-12-08 12:57:41 -0800

    Added Graph.contains function. Added Graph.shortestPath function.

--- Graph/Graph.swift
+++ Graph/Graph.swift
@@ -6,12 +6,18 @@
 //  Copyright © 2017 Kristofer Hanes. All rights reserved.
 //
 
+import Prelude
+
 public protocol Graph {
   associatedtype Edge: VertexConnecting
   func outgoingConnections(from vertex: Edge.Vertex) -> [Edge]?
 }
 
 public extension Graph where Edge.Vertex: Hashable {
+  
+  func contains(vertex: Edge.Vertex) -> Bool {
+    return outgoingConnections(from: vertex) != nil
+  }
   
   func depthFirstPath(from start: Edge.Vertex, to finish: Edge.Vertex) -> [Edge.Vertex]? {
     
@@ -22,29 +28,60 @@ public extension Graph where Edge.Vertex: Hashable {
       }
       visited.insert(vertex)
       for edge in outgoingConnections(from: vertex) ?? [] {
-        let newPath = LinkedList.connected(edge.to, path)
+        let newPath = LinkedList.prepending(edge.to, to: path)
         guard let path = search(vertex: edge.to, path: newPath, visited: &visited) else { continue }
         return path
       }
       return nil
     }
 
+    guard contains(vertex: start) else { return nil }
     var visited: Set<Edge.Vertex> = []
-    return search(vertex: start, path: LinkedList.connected(start, LinkedList.empty), visited: &visited)
+    return search(vertex: start, path: LinkedList.prepending(start, to: LinkedList.empty), visited: &visited)
   }
   
   func breadthFirstPath(from start: Edge.Vertex, to finish: Edge.Vertex) -> [Edge.Vertex]? {
+    typealias Element = (vertex: Edge.Vertex, path: LinkedList<Edge.Vertex>)
+    guard contains(vertex: start) else { return nil }
     var visited: Set<Edge.Vertex> = []
-    var queue = DoubleArrayQueue<(vertex: Edge.Vertex, path: LinkedList<Edge.Vertex>)>()
-    queue.enqueue((start, LinkedList.connected(start, LinkedList.empty)))
+    var queue = DoubleArrayQueue<Element>()
+    queue.enqueue((start, LinkedList(start)))
     while let current = queue.dequeue() {
       if visited.contains(current.vertex) { continue }
       if current.vertex == finish {
         return Array(list: current.path).reversed()
       }
       for edge in outgoingConnections(from: current.vertex) ?? [] {
-        let newPath = LinkedList.connected(edge.to, current.path)
+        let newPath = LinkedList.prepending(edge.to, to: current.path)
         queue.enqueue((edge.to, newPath))
+      }
+      visited.insert(current.vertex)
+    }
+    return nil
+  }
+  
+}
+
+public extension Graph where Edge: Weighted, Edge.Vertex: Hashable, Edge.Weight == Int {
+  
+  func shortestPath(from start: Edge.Vertex, to finish: Edge.Vertex) -> [Edge.Vertex]? {
+    guard contains(vertex: start) else { return nil }
+    typealias Element = (vertex: Edge.Vertex, path: LinkedList<Edge.Vertex>)
+    var visited: Set<Edge.Vertex> = []
+    var costForVertex = [start:0]
+    var priorityQueue = Heap<Element, Int>()
+    priorityQueue.enqueue(element: (start, LinkedList(start)), priority: 0)
+    while let current = priorityQueue.dequeueNext() {
+      if visited.contains(current.vertex) { continue }
+      guard current.vertex != finish else {
+        return Array(list: current.path).reversed()
+      }
+      for edge in outgoingConnections(from: current.vertex) ?? [] {
+        if visited.contains(edge.to) { continue }
+        guard let vertexCost = curried(+) <^> costForVertex[current.vertex] <*> .pure(edge.weight) else { continue }
+        let newPath = LinkedList.prepending(edge.to, to: current.path)
+        costForVertex[edge.to] = vertexCost
+        priorityQueue.enqueue(element: (edge.to, newPath), priority: vertexCost)
       }
       visited.insert(current.vertex)
     }
