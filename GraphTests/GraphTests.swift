a6b47b3b99d7d1e19f36fcafd96a268c1a9f2d05
Kristofer Hanes <kristoferhanes@gmail.com>
2017-12-08 13:00:40 -0800

    Added example graph. Added test for Graph.shortestPath.

--- GraphTests/GraphTests.swift
+++ GraphTests/GraphTests.swift
@@ -10,27 +10,47 @@ import XCTest
 @testable import Graph
 
 class GraphTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  
+  let graph = DictionaryGraph<WeightedEdge<String, Int>>(connections:
+    [ "a" :
+      [ WeightedEdge(from: "a", to: "b", weight: 4)
+      , WeightedEdge(from: "a", to: "c", weight: 2)
+      ]
+    , "b" :
+      [ WeightedEdge(from: "b", to: "a", weight: 4)
+      , WeightedEdge(from: "b", to: "c", weight: 1)
+      , WeightedEdge(from: "b", to: "d", weight: 5)
+      ]
+    , "c" :
+      [ WeightedEdge(from: "c", to: "a", weight: 2)
+      , WeightedEdge(from: "c", to: "b", weight: 1)
+      , WeightedEdge(from: "c", to: "d", weight: 8)
+      , WeightedEdge(from: "c", to: "e", weight: 10)
+      ]
+    , "d" :
+      [ WeightedEdge(from: "d", to: "b", weight: 5)
+      , WeightedEdge(from: "d", to: "c", weight: 8)
+      , WeightedEdge(from: "d", to: "e", weight: 2)
+      , WeightedEdge(from: "d", to: "z", weight: 6)
+      ]
+    , "e" :
+      [ WeightedEdge(from: "e", to: "c", weight: 10)
+      , WeightedEdge(from: "e", to: "d", weight: 2)
+      , WeightedEdge(from: "e", to: "z", weight: 3)
+      ]
+    , "z" :
+      [ WeightedEdge(from: "z", to: "d", weight: 6)
+      , WeightedEdge(from: "z", to: "e", weight: 3)
+      ]
+    ])
+  
+  func testShortestPath() {
+    XCTAssertNil(graph.shortestPath(from: "non-existing node", to: "non-existing node"))
+    XCTAssertNil(graph.shortestPath(from: "non-existing node", to: "z"))
+    XCTAssertNil(graph.shortestPath(from: "a", to: "non-existing node"))
+    XCTAssertEqual(graph.shortestPath(from: "a", to: "z") ?? [], ["a", "c", "b", "d", "e", "z"])
+    XCTAssertEqual(graph.shortestPath(from: "z", to: "a") ?? [], ["z", "e", "d", "b", "c", "a"])
+    XCTAssertEqual(graph.shortestPath(from: "a", to: "b") ?? [], ["a", "c", "b"])
+  }
+  
 }
